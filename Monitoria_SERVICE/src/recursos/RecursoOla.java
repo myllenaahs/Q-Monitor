be487043d04dccc802cbf6ce63be6f8f46c016b1
Myllena <Denize@192.168.25.102>
2016-03-28 22:04:24 -0300

    serviço de listagem

--- Monitoria_SERVICE/src/recursos/RecursoOla.java
+++ Monitoria_SERVICE/src/recursos/RecursoOla.java
@@ -6,7 +6,6 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
@@ -26,7 +25,7 @@ public class RecursoOla {
 	@Consumes("application/json")
 	@Produces("application/json")
 	public List<Cliente> findUserByName(Cliente user) {
-
+	
 		logger.info("/get-byname: " + user.getNome());
 
 		String fullName = user.getNome().trim().toUpperCase();
@@ -35,6 +34,7 @@ public class RecursoOla {
 		logger.info("Participants: " + users.size() + "[" + users + "]");
 
 		return users;
+		
 	}
 
 	@GET
@@ -42,10 +42,5 @@ public class RecursoOla {
 		return "Olá Mundo! <br/> O parâmetro de \"Query\"enviado foi: " + parametro;
 	}
 
-	@GET
-	@Path("/{parametro}")
-	public String digaOlaPath(@PathParam("parametro") String parametro) {
-		return "Olá Mundo! <br/> O parâmetro de \"URI\" enviado foi: " + parametro;
-	}
 
 }
