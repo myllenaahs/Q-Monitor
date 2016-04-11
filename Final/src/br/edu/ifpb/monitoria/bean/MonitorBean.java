d186c2ad1b19f5b4c0f0f3d969df61a8621f7ef1
Myllena <Denize@192.168.25.102>
2016-04-11 00:24:12 -0300

    Adição do growl messages

--- Final/src/br/edu/ifpb/monitoria/bean/MonitorBean.java
+++ Final/src/br/edu/ifpb/monitoria/bean/MonitorBean.java
@@ -1,24 +1,27 @@
 package br.edu.ifpb.monitoria.bean;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.context.RequestContext;
 
 import br.edu.ifpb.monitoria.entidade.Monitor;
 import br.edu.ifpb.monitoria.service.MonitoriaService;
 import br.edu.ifpb.monitoria.service.ProviderServiceFactory;
 
-@ManagedBean (name = "monitorBean")
+@ManagedBean(name = "monitorBean")
 @RequestScoped
 public class MonitorBean {
 
 	private Monitor monitor;
 	private List<Monitor> monitores;
-	
-	
-	
+
 	public List<Monitor> getMonitores() {
 		return monitores;
 	}
@@ -31,22 +34,20 @@ public class MonitorBean {
 		this.monitor = new Monitor();
 		this.monitores = new ArrayList<Monitor>();
 	}
-	
+
 	public String listar() {
-		
-		MonitoriaService service = ProviderServiceFactory
-				.createServiceClient(MonitoriaService.class);
-		
+
+		MonitoriaService service = ProviderServiceFactory.createServiceClient(MonitoriaService.class);
+
 		monitores = service.pesquisarMonitorDisciplina(monitor);
 
 		return null;
 	}
-	
+
 	public String cadastrar() {
-		
-		MonitoriaService service = ProviderServiceFactory
-				.createServiceClient(MonitoriaService.class);
-		
+
+		MonitoriaService service = ProviderServiceFactory.createServiceClient(MonitoriaService.class);
+
 		monitores = service.cadastrarMonitor(monitor);
 
 		return "Xhtml/index.xhtml";
@@ -59,4 +60,17 @@ public class MonitorBean {
 	public void setMonitor(Monitor monitor) {
 		this.monitor = monitor;
 	}
+
+	public void exibirMsgMonitor() {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Êxito",
+				"Monitor(a) cadastrado(a) com sucesso.");
+		RequestContext.getCurrentInstance().showMessageInDialog(message);
+		try {
+			FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
